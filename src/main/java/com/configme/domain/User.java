aa8a7e721338feddf777b54a6191e0d8bbd3433d
cleogithub <survleo@hotmail.fr>
2021-09-20 16:07:54 +0200

    fix authorities fetch auto

--- src/main/java/com/configme/domain/User.java
+++ src/main/java/com/configme/domain/User.java
@@ -94,7 +94,7 @@ public class User extends AbstractAuditingEntity implements Serializable {
     private Set<ClientConfig> clientConfigs = new HashSet<>();
 
     @JsonIgnore
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
         name = "jhi_user_authority",
         joinColumns = { @JoinColumn(name = "user_id", referencedColumnName = "id") },
