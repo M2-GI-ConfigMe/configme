752619adf39c6e5fa8543280702e2751a4861ae8
haskaris <mathis.perrier73@gmail.com>
2021-09-22 10:30:31 +0200

    Ajout des mails d'inscription

--- src/main/webapp/app/account/register/register.service.ts
+++ src/main/webapp/app/account/register/register.service.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export default class RegisterService {
   public processRegistration(account: any): Promise<any> {
+    account.langKey = 'fr';
+
     return axios.post('api/register', account);
   }
 
